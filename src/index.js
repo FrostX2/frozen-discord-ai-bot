d3d194f9f2159fa679ce1a92c4b486fa73d39a0b
NotFrost <frostx.apk@gmail.com>
2026-05-17 19:54:11 +0700

    Add Express web server for Render health check

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
+import express from 'express';
 import { Client, GatewayIntentBits } from 'discord.js';
 import { config } from './config.js';
 import { setupDiscordHandlers } from './discord.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bot is alive and running!');
+});
+
+app.listen(port, () => {
+  console.log(`Web server listening on port ${port}`);
+});
 
 const client = new Client({
   intents: [
